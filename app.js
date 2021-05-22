8569c77b267c42fbee5151ecf7fcbf3e1a12f1a3
chienhowh <63964025+chienhowh@users.noreply.github.com>
2021-05-22 21:40:41 +0800

    fix:80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors')
-const port = 3000;
+const port = 80;
 require('dotenv/config');
 
 // router start 
