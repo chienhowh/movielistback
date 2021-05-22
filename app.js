aff9c227446b22d8dc9413ff1fde3c127f70f239
chienhowh <63964025+chienhowh@users.noreply.github.com>
2021-05-22 21:44:00 +0800

    443

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors')
-const port = 80;
+const port = 443;
 require('dotenv/config');
 
 // router start 
