c1cdebc944507abef0a82806c862d66e5e73caaa
chienhowh <63964025+chienhowh@users.noreply.github.com>
2021-06-01 11:10:25 +0800

    back

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ const app = express();
 const cors = require('cors')
 const port = 80;
 const fs = require('fs')
+const https = require('https');
+const http = require('http');
 require('dotenv/config');
 // router start 
 const movieRouter = require('./routes/movie');
 // SSL
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/movieback.duckdns.org/privkey.pem','utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/movieback.duckdns.org/fullchain.pem','utf8');
-const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: certificate
-};
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/movieback.duckdns.org/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/movieback.duckdns.org/fullchain.pem', 'utf8');
+// const credentials = {
+//     key: privateKey,
+//     cert: certificate,
+//     ca: certificate
+// };
 
 /* middlewares start */
 app.use(cors());
@@ -29,9 +31,9 @@ app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-app.get('/.well-known/acme-challenge/PzjdoiaGuwni8S333_pIKEeegBuSfcHRffX1qjl4quY', (req, res) => {
-    res.send('PzjdoiaGuwni8S333_pIKEeegBuSfcHRffX1qjl4quY.RGRWCq5zUZyHAxcTdJLylg8d3hNXnVmEpJxjFUuTOvU')
-})
+// app.get('/.well-known/acme-challenge/PzjdoiaGuwni8S333_pIKEeegBuSfcHRffX1qjl4quY', (req, res) => {
+//     res.send('PzjdoiaGuwni8S333_pIKEeegBuSfcHRffX1qjl4quY.RGRWCq5zUZyHAxcTdJLylg8d3hNXnVmEpJxjFUuTOvU')
+// })
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
     console.log('connect to mongoDB');
@@ -41,7 +43,9 @@ const db = mongoose.connection;
 // 沒連上回傳錯誤
 db.on('error', console.error.bind(console), 'mongoDB error');
 
-app.listen(port, () => {
-    console.log('work')
-})
-// http.createServer(app).listen(port);
+// app.listen(port, () => {
+//     console.log('work')
+// })
+http.createServer(app).listen(port,()=>{
+    console.log('back work');
+});
